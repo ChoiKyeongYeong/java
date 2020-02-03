8bde5c2456a455b772e5bf9307d729b813417329
ChoiKyeongYeong <cky2662@naver.com>
2020-02-03 16:07:07 +0900

    자바 스케너

---
+++ Chapter1/Main.java
@@ -0,0 +1,13 @@
+package anyThing;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("아무 수를 입력하세요");
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		System.out.println(num);
+
+	}
+
+}
